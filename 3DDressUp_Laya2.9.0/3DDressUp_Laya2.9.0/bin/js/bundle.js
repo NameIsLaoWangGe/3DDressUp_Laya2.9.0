f55faabee872d0d74339b0169cbc9805670613b6
laowangge <849567502@qq.com>
2020-11-13 20:57:11 +0800

    裁剪表现完成

--- 3DDressUp_Laya2.9.0/3DDressUp_Laya2.9.0/bin/js/bundle.js
+++ 3DDressUp_Laya2.9.0/3DDressUp_Laya2.9.0/bin/js/bundle.js
@@ -4706,11 +4706,64 @@
 
     var _MakeClothes;
     (function (_MakeClothes) {
+        class Scissor extends Admin._Object {
+        }
+        _MakeClothes.Scissor = Scissor;
         class MakeClothes extends Admin._SceneBase {
+            constructor() {
+                super(...arguments);
+                this.Cutting = {
+                    Scissor: () => {
+                        return this._ImgVar('Scissor');
+                    },
+                    touchP: new Laya.Point(),
+                    diffP: new Laya.Point(),
+                    EraserSp: () => {
+                        if (!this._ImgVar('LineParent').getChildByName('EraserSp')) {
+                            let Sp = new Laya.Sprite();
+                            Sp.name = 'EraserSp';
+                            Sp.blendMode = "destination-out";
+                            this._ImgVar('LineParent').addChild(Sp);
+                            this._ImgVar('LineParent').cacheAs = "bitmap";
+                            return Sp;
+                        }
+                        else {
+                            return this._ImgVar('LineParent').getChildByName('EraserSp');
+                        }
+                    },
+                    EraserSize: 60,
+                    erasureLine: () => {
+                        let gPos = this.Cutting.Scissor().parent.localToGlobal(new Laya.Point(this._ImgVar('Scissor').x, this._ImgVar('Scissor').y));
+                        let localPos = this.Cutting.EraserSp().globalToLocal(gPos);
+                        this.Cutting.EraserSp().graphics.drawCircle(localPos.x, localPos.y, this.Cutting.EraserSize / 2, "#000000");
+                    }
+                };
+            }
             lwgBtnRegister() {
                 this._btnUp(this._ImgVar('BtnBack'), () => {
                     this._openScene(_SceneName.Start);
                 });
+                this._btnAll(this._ImgVar('Scissor'), () => {
+                }, () => {
+                }, () => {
+                }, () => {
+                });
+            }
+            onStageMouseDown(e) {
+                this.Cutting.touchP = new Laya.Point(e.stageX, e.stageY);
+            }
+            onStageMouseMove(e) {
+                if (this.Cutting.touchP) {
+                    this.Cutting.diffP.x = e.stageX - this.Cutting.touchP.x;
+                    this.Cutting.diffP.y = e.stageY - this.Cutting.touchP.y;
+                    this.Cutting.Scissor().x += this.Cutting.diffP.x;
+                    this.Cutting.Scissor().y += this.Cutting.diffP.y;
+                    this.Cutting.erasureLine();
+                    this.Cutting.touchP = new Laya.Point(e.stageX, e.stageY);
+                }
+            }
+            onStageMouseUp() {
+                this.Cutting.touchP = null;
             }
         }
         _MakeClothes.MakeClothes = MakeClothes;
--- 3DDressUp_Laya2.9.0/3DDressUp_Laya2.9.0/src/script/Lwg/_MakeClothes.ts
+++ 3DDressUp_Laya2.9.0/3DDressUp_Laya2.9.0/src/script/Lwg/_MakeClothes.ts
@@ -2,53 +2,59 @@ import { Admin, _SceneName } from "./Lwg";
 
 export module _MakeClothes {
     export class Scissor extends Admin._Object {
+        onTriggerEnter(): void {
 
+        }
     }
     export class MakeClothes extends Admin._SceneBase {
         lwgBtnRegister(): void {
             this._btnUp(this._ImgVar('BtnBack'), () => {
                 this._openScene(_SceneName.Start);
             })
-
-            this._btnAll(this._ImgVar('Scissor'),
-                () => {
-                },
-                () => {
-                },
-                () => {
-                },
-                () => {
-                })
         }
+
+        /**裁剪控制*/ 
         Cutting = {
-            Scissor: null as Laya.Image,
-            fP: null as Laya.Point,
-            diffP: null as Laya.Point,
-
-        }
-        onStageMouseDown(e: Laya.Event) {
-            if (!this.Cutting.fP) {
-                this.Cutting.fP.x = e.stageX;
-                this.Cutting.fP.y = e.stageY;
+            Scissor: () => {
+                return this._ImgVar('Scissor');
+            },
+            touchP: new Laya.Point(),
+            diffP: new Laya.Point(),
+            EraserSp: (): Laya.Sprite => {
+                if (!this._ImgVar('LineParent').getChildByName('EraserSp')) {
+                    let Sp = new Laya.Sprite();
+                    Sp.name = 'EraserSp';
+                    Sp.blendMode = "destination-out";
+                    this._ImgVar('LineParent').addChild(Sp);
+                    this._ImgVar('LineParent').cacheAs = "bitmap";
+                    return Sp;
+                } else {
+                    return this._ImgVar('LineParent').getChildByName('EraserSp') as Laya.Sprite;
+                }
+            },
+            EraserSize: 60,
+            erasureLine: () => {
+                let gPos = (this.Cutting.Scissor().parent as Laya.Sprite).localToGlobal(new Laya.Point(this._ImgVar('Scissor').x, this._ImgVar('Scissor').y));
+                let localPos = this.Cutting.EraserSp().globalToLocal(gPos);
+                this.Cutting.EraserSp().graphics.drawCircle(localPos.x, localPos.y, this.Cutting.EraserSize / 2, "#000000");
+                // this.Cutting.EraserSp.graphics.drawLine(this.Cutting.touchP.x, this.Cutting.touchP.y, localPos.x, localPos.y, "#000000", this.Cutting.EraserSize);
             }
         }
+        onStageMouseDown(e: Laya.Event) {
+            this.Cutting.touchP = new Laya.Point(e.stageX, e.stageY);
+        }
         onStageMouseMove(e: Laya.Event) {
-            if (this.Cutting.fP) {
-                // this.Cutting.diffP.x = e.stageX - this.Cutting.fP.x;
-                // this.Cutting.diffP.y = e.stageY - this.Cutting.fP.y;
-                // this.Cutting.Scissor.x = this._roomX + this.diffX;
-                // this.Cutting.Scissor.y = this._roomY + this.diffY;
+            if (this.Cutting.touchP) {
+                this.Cutting.diffP.x = e.stageX - this.Cutting.touchP.x;
+                this.Cutting.diffP.y = e.stageY - this.Cutting.touchP.y;
+                this.Cutting.Scissor().x += this.Cutting.diffP.x;
+                this.Cutting.Scissor().y += this.Cutting.diffP.y;
+                this.Cutting.erasureLine();
+                this.Cutting.touchP = new Laya.Point(e.stageX, e.stageY);
             }
         }
         onStageMouseUp() {
-            // this.selfScene['UIMain'].currentRoom = null;
-            // this._roomMove = false;
-            // this._roomX = null;
-            // this._roomY = null;
-            // this._stageX = null;
-            // this._stageY = null;
-            // this.diffX = null;
-            // this.diffY = null;
+            this.Cutting.touchP = null;
         }
     }
 }
